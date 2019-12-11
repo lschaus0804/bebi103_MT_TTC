e254bc4e9c0164493bef9638517f4ed8d2723164
lschaus0804 <lschaus@caltech.edu>
2019-12-11 11:53:49 -0800

    Added new function for generating tables

--- MT_TTC/MT_TTC/TTC_stats.py
+++ MT_TTC/MT_TTC/TTC_stats.py
@@ -25,6 +25,9 @@
             df_2: data frame from the second distribution to be tested
             mean_1: mean of the first distribution
             mean_2: mean of the second distribution
+			
+		df_TTC:
+		Returns table with the means and confidence intervals of TTC data (labeled and unlabeled)
         """
 
 #Preamb
@@ -52,21 +55,22 @@ def draw_bs_reps_mean(data, size=1):
         out[i] = np.mean(bs_reps(data))
     return out
 
-def mean_and_confidence(df):
+def mean_and_confidence(df, column = 'time to catastrophe (s)'):
 
-    # Number of bootstrap samples to be used (N.B.: will be used many times in 7.1)
-    n_samples = len(df)
+	# Number of bootstrap samples to be used 
+	n_samples = len(df)
 
-    # Get arithmetic means of the data
-    mean = np.mean(df)
-    
-    #Draw bootstrap samples and get the means for the bootstrap reps
-    mean_bs_reps = draw_bs_reps_mean(df, size = n_samples)
-    
-    # Calculate the confidence intervals
-    mean_conf_int = np.percentile(mean_bs_reps, [2.5, 97.5])
-    
-    return mean, mean_conf_int
+	# Get arithmetic means of the data
+	mean = np.mean(df[column].values)
+
+	#Draw bootstrap samples and get the means for the bootstrap reps
+	mean_bs_reps = draw_bs_reps_mean(df[column].values, size = n_samples)
+
+	# Calculate the confidence intervals
+	mean_conf_int = np.percentile(mean_bs_reps, [2.5, 97.5])
+
+
+	return mean, mean_conf_int
 
 
 def TTC_p_value(df_1, df_2, mean_1, mean_2, absolute=True):
@@ -93,3 +97,17 @@ def TTC_p_value(df_1, df_2, mean_1, mean_2, absolute=True):
     p_val = np.sum(perm_reps >= diff_mean) /len(perm_reps)
 
     return p_val
+	
+	
+def df_TTC(df_labeled, df_unlabeled):
+
+	#Get the means and confidence intervals
+	labeled_mean = mean_and_confidence(df_labeled)
+	unlabeled_mean = mean_and_confidence(df_unlabeled)
+	indexes = [True, False]
+	means = np.array([round(labeled_mean[0],2), round(unlabeled_mean[0],2)])
+	lower_bounds = np.array([round(labeled_mean[1][0],2), round(unlabeled_mean[1][0],2)])
+	upper_bounds = np.array([round(labeled_mean[1][1],2), round(unlabeled_mean[1][1],2)])
+	df = pd.DataFrame({'GFP label': indexes, 'Mean': means, 'Lower bound': lower_bounds, 'Upper bound': upper_bounds})
+	
+	return df
